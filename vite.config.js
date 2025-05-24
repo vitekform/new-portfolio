e100e7631fb342077bdd42519dac89875224ad2a
vitekform <vit.formanek.2022b@gypce.cz>
2025-05-24 11:04:11 +0200

    remove last trace of sentry from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "vit-formanek",
-    project: "web-frontend"
-  })],
+  plugins: [react()],
 
   build: {
     sourcemap: true
